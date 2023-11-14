94d62ee2c2c2e4490b4e66c906de06d918cefba2
jeffpham <jeffpham2906@gmail.com>
2023-11-14 17:44:48 +0700

    fix

--- controller/authController.js
+++ controller/authController.js
@@ -14,7 +14,7 @@ function createSendToken(user, statusCode, res) {
     const token = tokenSign(user._id)
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 1000 * 60 * 60 * 24),
-        httpOnly: true
+        // httpOnly: true
     }
     if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
     user.password = undefined
